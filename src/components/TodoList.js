894da4de9a14bcbcb0c9dba016e1a14b5b269599
user <kamo0915@gmail.com>
2022-11-29 18:37:25 +0900

    221129

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,23 +1,39 @@
+import React, { useCallback } from 'react';
+import { List } from 'react-virtualized';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
 //array.map(() => { 중괄호일 경우에는 return이 들어가야 함})
 //array.map(() => ( 괄호일 경우에는 return이 들어가지 않아도 됨))
 const TodoList = ({ todos, onToggle, onRemove }) => {
+  const rowRenderer = useCallback(
+    ({ index, key, style }) => {
+      const todo = todos[index];
+      return (
+        <TodoListItem
+          todo={todo}
+          key={key}
+          onToggle={onToggle}
+          onRemove={onRemove}
+          style={style}
+        ></TodoListItem>
+      );
+    },
+    [onRemove, onToggle, todos],
+  );
+
   return (
-    <div className="TodoList">
-      {todos.map((todo) => {
-        return (
-          <TodoListItem
-            todo={todo}
-            key={todo.id}
-            onToggle={onToggle}
-            onRemove={onRemove}
-          ></TodoListItem>
-        );
-      })}
-    </div>
+    <List
+      className="ToodoList"
+      width={512}
+      height={513}
+      rowCount={todos.length}
+      rowHeight={57}
+      rowRenderer={rowRenderer}
+      list={todos}
+      style={{ outline: 'none' }}
+    ></List>
   );
 };
 
-export default TodoList;
+export default React.memo(TodoList);
