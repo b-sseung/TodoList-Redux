e68e107ab974f39a514c5ad6d2f1a00f0af02261
user <kamo0915@gmail.com>
2023-01-14 20:40:19 +0900

    230114

--- src/App.js
+++ src/App.js
@@ -1,57 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState, useRef, useCallback } from 'react';
-import TodoTemplate from './components/TodoTemplate';
-import TodoInsert from './components/TodoInsert';
-import TodoList from './components/TodoList';
-
-function createBulkTodos() {
-  const array = [];
-  for (let i = 1; i <= 2500; i++) {
-    array.push({
-      id: i,
-      text: `할 일 ${i}`,
-      checked: false,
-    });
-  }
-  return array;
-}
+import TodosContainer from './containers/TodosContainer';
+import './scss/TodoTemplate.scss';
+import './scss/TodoList.scss';
+import './scss/TodoListItem.scss';
+import './scss/TodoInsert.scss';
 
 const App = () => {
-  const [todos, setTodos] = useState(createBulkTodos);
-
-  const nextId = useRef(2501);
-  const onInsert = useCallback((text) => {
-    const todo = {
-      id: nextId.current,
-      text,
-      checked: false,
-    };
-    setTodos((todos) => todos.concat(todo));
-    nextId.current += 1;
-  }, []);
-
-  const onToggle = useCallback((id) => {
-    setTodos((todos) =>
-      todos.map((value) =>
-        value.id === id ? { ...value, checked: !value.checked } : value,
-      ),
-    );
-  }, []);
-
-  const onRemove = useCallback((id) => {
-    setTodos((todos) => todos.filter((value) => value.id !== id));
-  }, []);
-
   return (
-    <TodoTemplate>
-      <TodoInsert onInsert={onInsert}></TodoInsert>
-      <TodoList
-        todos={todos}
-        onToggle={onToggle}
-        onRemove={onRemove}
-      ></TodoList>
-    </TodoTemplate>
+    <div>
+      <TodosContainer></TodosContainer>
+    </div>
   );
 };
 
